80c057d5db131194505ab5456af440ce64e16ce1
Snorcky <snorcky.lylouille@gmail.com>
2017-03-22 13:54:27 +0100

    Ajout world & msg

--- WarIsland/src/fr/snorcky/WarIsland/Commandes.java
+++ WarIsland/src/fr/snorcky/WarIsland/Commandes.java
@@ -2,12 +2,15 @@ package fr.snorcky.WarIsland;
 
 import java.util.Random;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.entity.Player;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class Commandes implements CommandExecutor {
 	
@@ -28,14 +31,17 @@ public class Commandes implements CommandExecutor {
 			if(cmd.getName().equalsIgnoreCase("wi")){
 				if(args[0].equalsIgnoreCase("create")){
 					Random random = new Random();
+					String colortp = config.getString("messages.teleport-message");
+					String colormsgtp = ChatColor.translateAlternateColorCodes('&', colortp);
 					
 					int x = p.getLocation().getBlockX() + random.nextInt(config.getInt("coords.x"));
 					int y = p.getLocation().getBlockY() + random.nextInt(config.getInt("coords.y"));
 					int z = p.getLocation().getBlockZ() + random.nextInt(config.getInt("coords.z"));
-					
-					Location aleatoire = new Location(p.getWorld(), x,y,z);
-					
+					Location aleatoire = new Location(Bukkit.getWorld(config.getString("world.world-name")), x,y,z);
 					p.teleport(aleatoire);
+					
+					
+					p.sendMessage(colormsgtp);
 					
 				}
 			}
